884c1f99a81dd2ba8f70573303afd39ce0deba53
mpjansson <pino.jansson@geursa.es>
2023-03-05 00:49:05 +0000

    AjustarVistaInicial

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+
+import {fromLonLat} from 'ol/proj';
+import sync from "ol-hashed";
+
+const center_4326=fromLonLat([-3.66667,40.5])
 const spainExtent = [-4242152.2315, 2253314.7512, 3506727.948, 6152214.69];
 
 const map = new Map({
@@ -12,7 +17,9 @@ const map = new Map({
     })
   ],
   view: new View({
-    center: [-500000, 4800000],
+    center: center_4326,
     zoom:6,
   })
 });
+
+sync(map)
