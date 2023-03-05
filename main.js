8c26f3856c111dc03319cf1dd0b4005e30eff6ff
mpjansson <pino.jansson@geursa.es>
2023-03-05 00:18:52 +0000

    Hashed_LayerSwitcher_npmInstal

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-
+const spainExtent = [-4242152.2315, 2253314.7512, 3506727.948, 6152214.69];
 
 const map = new Map({
   target: 'map',
@@ -13,6 +13,6 @@ const map = new Map({
   ],
   view: new View({
     center: [-500000, 4800000],
-    zoom: 6,
+    zoom:6,
   })
 });
