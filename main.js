93dd24381286a94e5c02ed6428272e7d789e9c32
mpjansson <pino.jansson@geursa.es>
2023-03-05 03:19:03 +0000

    Overview_ZoomCorunia

--- main.js
+++ main.js
@@ -6,8 +6,50 @@ import OSM from 'ol/source/OSM';
 import {fromLonLat} from 'ol/proj';
 import sync from "ol-hashed";
 
-const center_4326=fromLonLat([-3.66667,40.5])
+// Controles
+import {
+  ScaleLine,
+  MousePosition,
+  ZoomToExtent,
+  OverviewMap,
+  FullScreen,
+  defaults as defaultControls,
+  Control,
+} from "ol/control";
+
+const center_4326=fromLonLat([-3.6667,40.5])
 const spainExtent = [-4242152.2315, 2253314.7512, 3506727.948, 6152214.69];
+const corunia_extent=[-1051943, 5103303, -819030, 5467062];
+
+// 03.1 OverviewMap control
+
+const overviewMapControl = new OverviewMap({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+});
+
+// 03.2 Zoom extent contol
+
+const zoomToCoruniaControl = new ZoomToExtent({
+   extent: corunia_extent,
+ });
+
+
+//Controles EXTEND activados
+const extendControls = [
+  overviewMapControl,
+  //fullScreenControl,
+  zoomToCoruniaControl,
+  // mousePositionControl,
+  // scaleControl,
+  // scaleControl2,
+  // infoControl
+];
+
+//Map
 
 const map = new Map({
   target: 'map',
@@ -19,7 +61,14 @@ const map = new Map({
   view: new View({
     center: center_4326,
     zoom:6,
-  })
+    extent:spainExtent,
+  }),
+  controls: defaultControls({
+    // Gestión de los controles por defecto
+    zoom: true,
+    attribution: true,
+    rotate: true,
+  }).extend(extendControls),
 });
 
 sync(map)
